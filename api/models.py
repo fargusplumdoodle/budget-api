d920e8b6ff90b6202c67ba4018965cef9bc92123
fargus <fargusplumdoodle@gmail.com>
2019-10-20 20:21:08 -0700

    finished load script

--- api/models.py
+++ api/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Budget(models.Model):
-    name = models.TextField(max_length=20)
-    percentage = models.FloatField(max_length=0.2)
-    initial_balance = models.FloatField(max_length=4000)
+    name = models.TextField(max_length=20, unique=True)
+    percentage = models.FloatField(max_length=0.2, null=True)
+    initial_balance = models.FloatField(max_length=4000, null=True)
     current_balance = models.FloatField(max_length=4000, null=True)
     monthly_contribution = models.FloatField(max_length=4000, null=True)
 
@@ -15,7 +15,7 @@ class Budget(models.Model):
 class Transaction(models.Model):
     amount = models.FloatField(max_length=4000)
     description = models.TextField(max_length=300, null=True)
-    category = models.ForeignKey(Budget, null=True, on_delete=models.SET_NULL)
+    budget = models.ForeignKey(Budget, null=True, on_delete=models.SET_NULL)
     date = models.DateField()
 
     def __str__(self):
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.core.management import call_command
+from .management.commands.load_transactions import Command as LoadTransaction
 
-# Create your tests here.
+
+class load_transaction(TestCase):
+    def test_validate_csv(self):
+
+        lt = LoadTransaction()
+
+        # trying valid input
+        fields = [x for x in lt.REQUIRED_FIELDS]
+        assert lt.invalid_csv_headers(fields) is None
+
+        # trying one less field
+        fields.pop()
+        assert lt.invalid_csv_headers(fields) is not None
+
+
