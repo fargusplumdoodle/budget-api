7a587d36ef9914d1e4641230aa5d2adbdc8197b9
fargus plumdoodle <fargusplumdoodle@gmail.com>
2019-11-20 19:55:54 -0800

    registered profile in admin interface

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 
-@admin.register(Transaction, Budget)
+@admin.register(Transaction, Budget, Profile)
 class BudgetAdmin(admin.ModelAdmin):
     pass
