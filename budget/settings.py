d976de6b3d7c6399ffddd8f08cafb5df1c881eb7
fargus plumdoodle <fargusplumdoodle@gmail.com>
2020-05-27 12:42:41 -0700

    'app now uses environment variables or environment file'

--- budget/settings.py
+++ budget/settings.py
@@ -11,7 +11,18 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from .environment import DEBUG, SECRET_KEY, DB_HOST, DB, DB_USER, DB_PASS
+
+try:
+    from budget.environment import DEBUG, SECRET_KEY, DB_HOST, DB, DB_USER, DB_PASS
+    print("Read variables from environment file")
+except ImportError:
+    DEBUG = os.getenv("DEBUG") == "TRUE"
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    DB_HOST = os.getenv("DB_HOST")
+    DB = os.getenv("DB")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    print("Read variables from environment variables")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,16 +85,16 @@ WSGI_APPLICATION = "budget.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
-"default": {
-    "ENGINE": "django.db.backends.postgresql",
-    "NAME": DB,
-    "USER": DB_USER,
-    "PASSWORD": DB_PASS,
-    "HOST": DB_HOST,
-    "PORT": 5432,
-    "ATOMIC_REQUESTS": True,
-    "CONN_MAX_AGE": 0,
-}
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": DB,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASS,
+        "HOST": DB_HOST,
+        "PORT": 5432,
+        "ATOMIC_REQUESTS": True,
+        "CONN_MAX_AGE": 0,
+    }
 }
 
 # Password validation
