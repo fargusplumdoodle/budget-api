06735c373ef032088b36bb847b6b0954e5df52ea
fargus plumdoodle <fargusplumdoodle@gmail.com>
2020-09-27 08:34:19 -0700

    Date queries now match equal to

--- api2/filters.py
+++ api2/filters.py
@@ -15,10 +15,13 @@ class TransactionFilterset(filters.FilterSet):
     budget = filters.CharFilter(field_name="budget__id", lookup_expr="exact")
     budget__name = filters.CharFilter(field_name="budget__name", lookup_expr="exact")
 
-    description__exact = filters.CharFilter(field_name="description", lookup_expr="exact")
-    description__icontains = filters.CharFilter(field_name="description", lookup_expr="icontains")
+    description__exact = filters.CharFilter(
+        field_name="description", lookup_expr="exact"
+    )
+    description__icontains = filters.CharFilter(
+        field_name="description", lookup_expr="icontains"
+    )
 
     date = filters.DateFilter(field_name="date")
-    date__gt = filters.DateFilter(field_name="date", lookup_expr="gt")
-    date__lt = filters.DateFilter(field_name="date", lookup_expr="lt")
-
+    date__gte = filters.DateFilter(field_name="date", lookup_expr="gte")
+    date__lte = filters.DateFilter(field_name="date", lookup_expr="lte")
