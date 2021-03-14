5af9e76ca52b26d4683eb01b3a50a4dc5006948d
fargus plumdoodle <fargusplumdoodle@gmail.com>
2021-03-14 13:17:50 -0700

    7: Added stdout for calculate-income-outcome command

--- api2/management/commands/calculate_income_outcome.py
+++ api2/management/commands/calculate_income_outcome.py
@@ -13,16 +13,14 @@ class Command(BaseCommand):
     that budget
     """
 
-    def add_arguments(self, parser):
-        # path to the csv containing transactions. Must exist
-        parser.add_argument(
-            "--save", action="save_true", help="Save changes to database"
-        )
-
     def handle(self, *args, **options):
-        self.calculate_income_outcome(save=options["save"])
-
-    @staticmethod
-    def calculate_income_outcome(save=False):
         for budget in Budget.objects.all():
-            budget.calculate_income_outcome(save=save)
+            budget.calculate_income_outcome(save=True)
+            self.stdout.write(
+                self.style.SUCCESS(
+                    'Budget "%s"\n'
+                    "  - income: %s\n"
+                    "  - outcome: %s\n"
+                    % (budget.name, budget.income_per_month, budget.outcome_per_month)
+                )
+            )
