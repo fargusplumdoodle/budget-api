c3c03a6aa15e9f59753c6dff323f0d2e12695593
fargus plumdoodle <fargusplumdoodle@gmail.com>
2022-01-02 08:33:28 -0800

    Pagination is optional

--- budget/pagination.py
+++ budget/pagination.py
@@ -1,7 +1,14 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class VariablePageSizePagination(PageNumberPagination):
     page_size = 25
     max_page_size = 100
     page_size_query_param = "page_size"
+
+    def get_paginated_response(self, data) -> Response:
+        if self.request.GET.get("no_pagination"):
+            return Response(data)
+
+        return super().get_paginated_response(data)
