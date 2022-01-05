e08335a3dfd696738ca725d66ef10183e63265e6
fargus plumdoodle <fargusplumdoodle@gmail.com>
2022-01-04 23:10:21 -0800

    'optimized ci process'

--- scripts/install-ci.sh
+++ scripts/install-ci.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -ex
-apt update && apt install build-essential -y 
-
-pip install -U pip pipenv
-pipenv lock \
+python -m venv ./venv
+./venv/bin/pip install -U pip pipenv
+./venv/bin/pipenv lock \
 	--dev \
 	--keep-outdated \
 	--requirements > /requirements.txt 
-pip install -U pip install -r /requirements.txt
+
